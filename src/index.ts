9ed842ae0612575bbce29ba1528d0d39c60eb16c
Mukesh Yadav <mukeshyadav@gmail.com>
2020-06-22 20:43:36 +0530

    adding more type of variable declaration in typescript, with description

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,34 @@
 import Vehicle from "./classDefination";
-const myString: string = "Hello World";
 
 const myVehicle: Vehicle = new Vehicle(4, 200);
 myVehicle.accelerate(20);
-console.log(myVehicle.speed);
 
-//number
+/**
+ * Typescript, all number are floating point,
+ * its supports decimal, hexadecimal, octal and binary literal
+ * @type {number}
+ */
 let varNum: number = 1;
 
-// string
-let vanString: string = "Mukesh";
+/**
+ * Typescript, follows the same syntax as Javascript
+ * @type {string}
+ */
+let varString: string = "Mukesh";
 
-//boolean
+/**
+ * Boolean values function just like they do in Javascript
+ * @type {boolean}
+ */
 let varTrue: boolean = true;
+
+/**
+ * In Typescript, arrays are a collection of same object
+ * Array can be declared in two way with datatype follow by []
+ * or the standard approach with Array<elemType>
+ * Also can declare array with multiple types using | operator or
+ * create multidimensional array to save one into another using []
+ * @type {Array.<number>}
+ */
+let varArray: Array<number> = [1, 2, 3];
+let varArrayMix: (Date | string[])[] = [new Date(), new Date(), ["1", "abc"]];
