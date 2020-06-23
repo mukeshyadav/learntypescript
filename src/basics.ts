884ccbe20a09b3d52619bfd3f184eaa8cd5cb08d
Mukesh Yadav <mukeshyadav@gmail.com>
2020-06-23 20:43:25 +0530

    adding basic file for typescript definition

---
+++ src/basics.ts
@@ -0,0 +1,41 @@
+/**
+ * Typescript, all number are floating point,
+ * its supports decimal, hexadecimal, octal and binary literal
+ * @type {number}
+ */
+let varNum: number = 1;
+
+/**
+ * Typescript, follows the same syntax as Javascript
+ * @type {string}
+ */
+let varString: string = "Mukesh";
+
+/**
+ * Boolean values function just like they do in Javascript
+ * @type {boolean}
+ */
+let varTrue: boolean = true;
+
+/**
+ * In Typescript, arrays are a collection of same object
+ * Array can be declared in two way with datatype follow by []
+ * or the standard approach with Array<elemType>
+ * Also can declare array with multiple types using | operator or
+ * create multidimensional array to save one into another using []
+ * @type {Array.<number>}
+ */
+let varArray: Array<number> = [1, 2, 3];
+let varArrayMix: (Date | string[])[] = [new Date(), new Date(), ["1", "abc"]];
+
+/**
+ * In Typescript, Tuple are like arrays, but there we can define type of data at each index
+ * @type {tuple}
+ */
+
+let varNumberTuple: [number, number, number] = [1, 2, 3];
+
+/**
+ * Void, is subtype of undefine. It is return type that is substituted with different types
+ * when needed.
+ */
